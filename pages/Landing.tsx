7817850fc38902307d4b3f1693c69d89e1c0ca83
Valiyev Kamron <valiyevkamron201@gmail.com>
2026-02-22 13:15:56 +0500

    Update Landing.tsx

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useUI } from "../context/UIContext";
+import { useAuth } from "../context/AuthContext";
+import { Role } from "../types";
 
 const Landing: React.FC = () => {
   const navigate = useNavigate();
+  const { isAuthenticated, user } = useAuth();
 
-  // Avtomatik ravishda login sahifasiga yo'naltirish
   React.useEffect(() => {
-    navigate("/login");
-  }, [navigate]);
+    if (isAuthenticated && user) {
+      if (user.role === Role.ADMIN) {
+        navigate("/admin");
+      } else {
+        navigate("/user");
+      }
+    } else {
+      navigate("/login");
+    }
+  }, [navigate, isAuthenticated, user]);
 
   return null;
 };
